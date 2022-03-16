396f2370fe10ebdb357beb5cd1863c5e42badc84
Max <kpabomakc@yandex.ru>
2022-03-17 01:15:38 +0200

    fix homeWork for racing with java + annotations config

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,15 @@
 package org.example;
 
-import org.example.services.RacingImpl;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.example.services.Racing;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-//        ApplicationContext context = new AnnotationConfigApplicationContext("org.example.config");
-//        context.getBean(Racing.class);
-    
-        AbstractApplicationContext context = new ClassPathXmlApplicationContext("horseRacing.xml");
-        RacingImpl bean = context.getBean(RacingImpl.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext("org.example.config");
+        Racing bean = context.getBean(Racing.class);
         bean.printRaceResult();
         
     }
